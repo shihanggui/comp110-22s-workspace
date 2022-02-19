05a1ce3fb8c15aa6543817e4dee3f97345e962d9
Shihang Gui <shihang@ad.unc.edu>
2022-02-19 15:22:21 -0500

    progress on ex03

---
+++ exercises/ex05/utils.py
@@ -0,0 +1,36 @@
+"""List Utility Functions."""
+
+__author__ = "730431294"
+
+
+def only_evens(original_list: list[int]) -> list[int]:
+    new_list: list[int] = []
+    for i in original_list:
+        if i % 2 == 0:
+            new_list.append(i)
+    return new_list
+
+
+def sub(a_list: list[int], start: int, end: int) -> list[int]:
+    new_list: list[int] = []
+    if len(a_list) == 0 or start > len(a_list) or end <= 0:
+        return new_list
+    else:
+        if start < 0:
+            start = 0
+        if end > len(a_list):
+            end = len(a_list)
+        i: int = start
+        while i < end:
+            new_list.append(a_list[i])
+            i += 1
+        return new_list
+
+
+def concat(list_a: list[int], list_b: list[int]) -> list[int]:
+    new_list: list[int] = []
+    for i in list_a:
+        new_list.append(i)
+    for j in list_b:
+        new_list.append(j)
+    return new_list
---
+++ exercises/ex05/utils_test.py
@@ -0,0 +1,59 @@
+"""Unit test for list utility functions."""
+
+__author__ = "730431294"
+
+from exercises.ex05.utils import only_evens, sub, concat
+
+
+def test_only_evens_empty() -> None:
+    a_list: list[int] = []
+    assert only_evens(a_list) == []
+
+
+def test_only_evens_all_evens() -> None:
+    a_list: list[int] = [2, 4, 6, 8]
+    assert only_evens(a_list) == [2, 4, 6, 8]
+
+
+def test_only_evens_all_odds() -> None:
+    a_list: list[int] = [1, 3, 5, 7]
+    assert only_evens(a_list) == []
+
+
+def test_sub_empty() -> None:
+    a_list: list[int] = []
+    start: int = 1
+    end: int = -1
+    assert sub(a_list, start, end) == []
+
+
+def test_sub_full_list() -> None:
+    a_list: list[int] = [1, 2, 4, 5]
+    start: int = -1
+    end: int = len(a_list) + 1
+    assert sub(a_list, start, end) == [1, 2, 4, 5]
+
+
+def test_sub_random() -> None:
+    a_list: list[int] = [21, 24, 16, 13]
+    start: int = 1
+    end: int = 3
+    assert sub(a_list, start, end) == [24, 16]
+
+
+def test_concat_one_empty() -> None:
+    list_a: list[int] = []
+    list_b: list[int] = [2, 3, 5]
+    assert concat(list_a, list_b) == [2, 3, 5]
+
+
+def test_concat_random() -> None:
+    list_a: list[int] = [3, 5, 7]
+    list_b: list[int] = [2, 3, 5]
+    assert concat(list_a, list_b) == [3, 5, 7, 2, 3, 5]
+
+
+def test_concat_random_converse() -> None:
+    list_b: list[int] = [3, 5, 7]
+    list_a: list[int] = [2, 3, 5]
+    assert concat(list_a, list_b) == [2, 3, 5, 3, 5, 7]
